03850063878b40beb9dd32519c9b8c817533746c
K0Re3 <dawid.korybalski@gmail.com>
2019-08-30 11:47:29 +0200

    website body

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,294 @@
 import React from 'react'
+import styled from 'styled-components'
+import { graphql, StaticQuery } from 'gatsby'
+import Navbar from '../components/navbar.js'
 
-export default () => <div>Hello world! New project in comming!</div>
+import Img from 'gatsby-image'
+
+import BackgroundImage from 'gatsby-background-image'
+
+const ourOfferts = [
+  {
+    name: 'Wulkanizacja',
+    description:
+      'Wymienimy Twoje opony i wyważymy koła. Przechowamy Twoje opony do następnego sezonu.',
+    imageId: 'opony',
+  },
+  {
+    name: 'Naprawa i regeneracja silników',
+    description:
+      'Silnik jest sercem Twojego samochodu. Zadbaj o niego zostawiając swoje auto w naszym serwisie.',
+    imageId: 'silnik',
+  },
+  {
+    name: 'Diagnostyka i naprawa zawieszenia',
+    description:
+      'Dzięki dobremu zawieszeniu możesz poczuć się komfortowo i bezpiecznie. To naprawdę ważne, tym bardziej na polskich drogach.',
+    imageId: 'zawieszenie',
+    imageAlt: 'Ilustracja przedstwiająca zawieszenie',
+  },
+  {
+    name: 'Przeglądy okresowe',
+    description:
+      'Wymienimy klocki i tarcze hamulcowe oraz oleje i filtry w Twoim samochodzie.',
+    imageId: 'przeglad',
+  },
+  {
+    name: 'Regeneracja alternatorów',
+    description:
+      'Jeśli coś zaczyna popiskiwać pod Twoją maską, to szybciutko należy sprawdzić stan alternatora, zanim nadejdą większe szkody.',
+    imageId: 'alternator',
+  },
+  {
+    name: 'Spawalnictwo',
+    description: 'To nasza specjalność. Pospawamy prawie wszystko.',
+    imageId: 'spawalnictwo',
+  },
+]
+
+const ourPros = [
+  {
+    name: 'Jakość',
+    description:
+      'Mamy nowoczesny warsztat i doświadczonych mechaników. Oferujemy Ci usługi na najwyższym poziomie.',
+    imageId: 'jakosc',
+  },
+  {
+    name: 'Odpowiedzialność',
+    description:
+      'Twój samochód będzie przygotowany na każdą podróż. Gotowy na przygodę?',
+    imageId: 'odpowiedzialnosc',
+  },
+  {
+    name: 'Czas naprawy',
+    description:
+      'Naprawiamy tak szybko, jak to możliwe. I zawsze zgodnie z terminem.',
+    imageId: 'czas_naprawy',
+  },
+  {
+    name: 'Uczciwość',
+    description:
+      'Jesteśmy uczciwi wobec Ciebie. Robimy dokładnie to, na co się umawiamy.',
+    imageId: 'uczciwosc',
+  },
+  {
+    name: 'Doświadczenie',
+    description:
+      'Już od wielu lat dbamy o pojazdy. Pracują u nas doświadczeni serwisanci, którzy naprawdę znają się na rzeczy.',
+    imageId: 'doswiadczenie',
+  },
+]
+
+const OurProLink = styled.a`
+  color: initial;
+  text-decoration: none;
+  background: hsla(0, 0%, 0%, 0.02);
+  border-radius: 0.2rem;
+  display: flex;
+  flex-flow: row wrap;
+`
+
+const OurProImage = styled(Img)`
+  width: 21.2rem;
+  height: 30rem;
+  flex: 0 0 auto;
+`
+
+const OurProContent = styled.div`
+  padding: 2rem;
+`
+
+const OurPro = ({ name, description, imageId, images }) => {
+  return (
+    <OurProLink>
+      <OurProImage fixed={images[imageId].childImageSharp.fixed} />
+      <OurProContent>
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </OurProContent>
+    </OurProLink>
+  )
+}
+
+const OurOffertLink = styled.a`
+  color: initial;
+  text-decoration: none;
+  background: hsla(0, 0%, 0%, 0.02);
+  border-radius: 0.2rem;
+  display: flex;
+  flex-flow: row wrap;
+`
+
+const OurOffertImage = styled(Img)`
+  width: 21.2rem;
+  height: 30rem;
+  flex: 0 0 auto;
+`
+
+const OurOffertContent = styled.div`
+  padding: 2rem;
+`
+const OurOffert = ({ name, description, imageId, images }) => {
+  return (
+    <OurOffertLink>
+      <OurOffertImage fixed={images[imageId].childImageSharp.fixed} />
+      <OurOffertContent>
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </OurOffertContent>
+    </OurOffertLink>
+  )
+}
+
+const IndexPage = ({ className }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "home_page.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        blur: file(relativePath: { eq: "home_blur.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        opony: file(relativePath: { eq: "opony.png" }) {
+          childImageSharp {
+            fixed(width: 207, height: 188) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        alternator: file(relativePath: { eq: "alternator.jpg" }) {
+          childImageSharp {
+            fixed(width: 245, height: 188) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        przeglad: file(relativePath: { eq: "przeglad.png" }) {
+          childImageSharp {
+            fixed(width: 283, height: 188) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        silnik: file(relativePath: { eq: "silnik.png" }) {
+          childImageSharp {
+            fixed(width: 235, height: 188) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        spawalnictwo: file(relativePath: { eq: "spawalnictwo.png" }) {
+          childImageSharp {
+            fixed(width: 250, height: 188) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        zawieszenie: file(relativePath: { eq: "zawieszenie.png" }) {
+          childImageSharp {
+            fixed(width: 282, height: 188) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        czas_naprawy: file(relativePath: { eq: "czas_naprawy.png" }) {
+          childImageSharp {
+            fixed(width: 32, height: 32) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        doswiadczenie: file(relativePath: { eq: "doswiadczenie.png" }) {
+          childImageSharp {
+            fixed(width: 32, height: 32) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        jakosc: file(relativePath: { eq: "jakosc.png" }) {
+          childImageSharp {
+            fixed(width: 32, height: 32) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        odpowiedzialnosc: file(relativePath: { eq: "odpowiedzialnosc.png" }) {
+          childImageSharp {
+            fixed(width: 32, height: 32) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+        uczciwosc: file(relativePath: { eq: "uczciwosc.png" }) {
+          childImageSharp {
+            fixed(width: 32, height: 32) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      return (
+        <div>
+          <Navbar />
+          <BackgroundImage
+            Tag='section'
+            className={className}
+            fluid={data.desktop.childImageSharp.fluid}>
+            <h1>Naprawdę możesz nam zaufać</h1>
+            <h2>
+              Szukasz dobrego mechanika? Dobrze trafiłeś, ponieważ należycie Cię
+              obsłużymy i jesteśmy uczciwi. A co najważniejsze, kochamy to, co
+              robimy i pragniemy, żebyś był zadowolony.
+            </h2>
+            <h2>Umów wizytę - button!</h2>
+          </BackgroundImage>
+          <div>
+            {ourPros.map(ourPro => (
+              <OurPro key={ourPro.name} {...ourPro} images={data} />
+            ))}
+          </div>
+          <BackgroundImage
+            Tag='section'
+            fluid={data.blur.childImageSharp.fluid}>
+            <h1>Naprawa samochodu nigdy nie była taka prosta</h1>
+            <h2>Umów wizytę - button!</h2>
+          </BackgroundImage>
+          <div>
+            <h1>Gallery???</h1>
+          </div>
+          <div>
+            <h1>Rzetelna wycena</h1>
+            <p>
+              Przed naprawą dowiesz jakie są przewidywane koszty naprawy oraz
+              ile ona potrwa.
+            </p>
+            <h2>Umów wizytę - button!</h2>
+          </div>
+          <div>
+            <h1>Co robimy?</h1>
+            {ourOfferts.map(ourOffert => (
+              <OurOffert key={ourOffert.name} {...ourOffert} images={data} />
+            ))}
+          </div>
+          <div>
+            <h1>Nie musisz szukać dalej</h1>
+            <h2>Umów wizytę - button!</h2>
+          </div>
+        </div>
+      )
+    }}
+  />
+)
+
+export default IndexPage
